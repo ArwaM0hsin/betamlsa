17a5b239b554b06b063143f893a7615f8e7033bf
Anusha Baig <160581223+ArwaM0hsin@users.noreply.github.com>
2024-02-27 01:38:53 +0500

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/png.png";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "this is beautiful image";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Smart irrigation system",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Designed smart irrigation system for the plants",
+    url: "https://github.com/Ayushparikh-code/Web-dev-mini-projects.git",
   },
   {
-    title: "Web Development for Beginners",
+    title: "smart calculator",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "used logics for building smart calculator",
+    url: "https://github.com/2KAbhishek/projects.git",
   },
   {
-    title: "My Resume Site",
+    title: "My Resume",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "www.linkedin.com/arwa.mohsin",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "this is my site",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    url: "https://github.com/akinmustafa/awesome-unity-games.git",
   },
 ];
 
