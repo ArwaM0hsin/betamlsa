79eef359631c9b090987a5908e67b00d674b71ec
Anusha Baig <160581223+ArwaM0hsin@users.noreply.github.com>
2024-02-27 01:53:54 +0500

    upd

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/pexels.jpg";
+import image from "../images/1.webp";
 
 const imageAltText = "Arwa Mohsin - Mechatronics Engineer - this is background pic";
 
