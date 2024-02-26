0d96eb9579e4b8bdadc60ce650992cd11b32d3f6
Anusha Baig <160581223+ArwaM0hsin@users.noreply.github.com>
2024-02-27 01:58:29 +0500

    sds

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/123.jpeg";
+import image from "../images/pexels.jpg";
 
 const imageAltText = "this is the background image";
 
