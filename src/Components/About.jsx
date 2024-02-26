30d7b378627b98af7e6ec9fbfd636c2097940637
Anusha Baig <160581223+ArwaM0hsin@users.noreply.github.com>
2024-02-27 01:49:03 +0500

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "this is the background image";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Mechatroincs Engineer at University of Engineering and Technology, Lahore";
 
 /**
  * List of some of skills or technologies you work on, are learning,
